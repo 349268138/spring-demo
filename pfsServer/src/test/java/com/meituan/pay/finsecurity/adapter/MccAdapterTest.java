92edb3153c31bb7526ad5d1d412ed20d5ba27537
hubei <hubei@meituan.com>
2020-11-06 15:28:39 +0800

    mccAdapter单测

--- pfsServer/src/test/java/com/meituan/pay/finsecurity/adapter/MccAdapterTest.java
+++ pfsServer/src/test/java/com/meituan/pay/finsecurity/adapter/MccAdapterTest.java
@@ -1,6 +1,10 @@
 package com.meituan.pay.finsecurity.adapter;
 
+import com.meituan.funds.simple.util.JacksonUtils;
 import com.meituan.pay.finsecurity.constant.MccConstant;
+import com.meituan.pay.finsecurity.po.*;
+import com.meituan.pay.finsecurity.po.enums.DataAccessTypeEnum;
+import com.meituan.pay.finsecurity.po.enums.TypeEnum;
 import com.sankuai.meituan.config.MtConfigClient;
 import org.junit.Assert;
 import org.junit.Before;
@@ -9,8 +13,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 /**
  * @author hhhb
@@ -29,19 +38,65 @@ public class MccAdapterTest {
     }
 
     @Test
-    public void getStringTest(){
+    public void initEventDataMapTest(){
+        Map<String, TradeEvent> eventDataMap = new HashMap<>();
+        TradeEvent tradeEvent = null;
 
-//        String value = null;
-//        value = mccAdapter.getString("unexist_key", "hb");
-//        Assert.assertEquals(null, value);
-//
-//        when(mtConfigClient.getValue(MccConstant.EVENTDATAMAP_KEY)).thenReturn("test");
-//        value = mccAdapter.getString(MccConstant.EVENTDATAMAP_KEY, "hb");
-//        Assert.assertEquals("test", value);
-//
-//        when(mtConfigClient.getValue(anyString())).thenThrow(new RuntimeException());
-//        value = mccAdapter.getString(MccConstant.EVENTDATAMAP_KEY, "hb");
-//        Assert.assertEquals("hb", value);
+        doReturn(null).when(mtConfigClient).getValue(MccConstant.EVENTDATAMAP_KEY);
+        mccAdapter.init();
+        Assert.assertTrue(mccAdapter.getEventDataMap().isEmpty());
+
+        doReturn("").when(mtConfigClient).getValue(MccConstant.EVENTDATAMAP_KEY);
+        mccAdapter.init();
+        Assert.assertTrue(mccAdapter.getEventDataMap().isEmpty());
+
+        doReturn("[").when(mtConfigClient).getValue(MccConstant.EVENTDATAMAP_KEY);
+        try {
+            mccAdapter.init();
+        } catch (Exception e) {
+            Assert.assertTrue(e instanceof RuntimeException);
+        }
+
+        List<DataRule> dataRuleList = new ArrayList<>();;
+        List<DecisionRule> decisionRuleList = new ArrayList<>();
+        List<Vector> vectorList = new ArrayList<>();
+        EventRule eventRule = new EventRule();
+        tradeEvent = new TradeEvent();
+        Vector vector = new Vector();
+        vector.setAlias("er_01");
+        vector.setName("事件规则01");
+        vector.setExpr("true");
+        vectorList.add(vector);
+        eventRule.setId(1L);
+        eventRule.setCode("fundsRequest");
+        eventRule.setName("eventRule_01");
+        eventRule.setVectorList(vectorList);
+        DataRule dataRule = new DataRule();
+        dataRule.setId(1L);
+        dataRule.setEventId(1L);
+        dataRule.setName("付款核心数据配置");
+        dataRule.setAlias("paycore");
+        dataRule.setAddress("com.sankuai.pay.fundstransfer.paycore:9006:localhost");
+        dataRule.setType(DataAccessTypeEnum.RPC);
+        dataRule.setKeyExpr("eventData.trade_no");
+        dataRuleList.add(dataRule);
+        DecisionRule decisionRule = new DecisionRule();
+        decisionRule.setId(1L);
+        decisionRule.setEventId(1L);
+        decisionRule.setName("决策规则_01");
+        decisionRule.setAlias("decisionRule_01");
+        decisionRule.setType(TypeEnum.ALARM);
+        decisionRule.setExpr("true");
+        decisionRuleList.add(decisionRule);
+        tradeEvent.setEventRule(eventRule);
+        tradeEvent.setDataRuleList(dataRuleList);
+        tradeEvent.setDecisionRuleList(decisionRuleList);
+        eventDataMap.put(eventRule.getCode(), tradeEvent);
+
+        doReturn(JacksonUtils.toJson(eventDataMap)).when(mtConfigClient).getValue(MccConstant.EVENTDATAMAP_KEY);
+        mccAdapter.init();
+        Assert.assertFalse(mccAdapter.getEventDataMap().isEmpty());
+
     }
 
 }

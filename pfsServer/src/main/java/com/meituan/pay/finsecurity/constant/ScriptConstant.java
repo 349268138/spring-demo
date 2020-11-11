91be130a7e3e2e8bb1f8b3300041c49532876cca
jinpingwang <wangjinping@meituan.com>
2020-11-11 11:37:15 +0800

    modify ScriptConstant

--- pfsServer/src/main/java/com/meituan/pay/finsecurity/constant/ScriptConstant.java
+++ pfsServer/src/main/java/com/meituan/pay/finsecurity/constant/ScriptConstant.java
@@ -14,6 +14,6 @@ public class ScriptConstant {
     public static final String CONTEXT_SCRIPT = "import groovy.json.*;import org.springframework.util.StringUtils;\n " +
             "jsonSlurper = new JsonSlurper()\n " +
             "contextData = jsonSlurper.parseText(contextData)\n " +
-            "if(StringUtils.isNotEmpty(contextData.eventData)) eventData = jsonSlurper.parseText(contextData.eventData)\n" +
-            "if(StringUtils.isNotEmpty(contextData.tradeData)) tradeData = jsonSlurper.parseText(contextData.tradeData)";
+            "if(!StringUtils.isEmpty(contextData.eventData)) eventData = jsonSlurper.parseText(contextData.eventData)\n" +
+            "if(!StringUtils.isEmpty(contextData.tradeData)) tradeData = jsonSlurper.parseText(contextData.tradeData)";
 }

fdee4e91606698e0fb5dbcac39afa00293e2e9aa
hubei <hubei@meituan.com>
2020-12-02 17:22:40 +0800

    mark

--- pfsServer/src/test/java/com/meituan/pay/finsecurity/adapter/DataQueryServiceAdapterIntegration.java
+++ pfsServer/src/test/java/com/meituan/pay/finsecurity/adapter/DataQueryServiceAdapterIntegration.java
@@ -15,6 +15,7 @@ import org.mockito.MockitoAnnotations;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.meituan.pay.finsecurity.constant.TradeDataConstant.*;
 import static org.mockito.Mockito.when;
 
 /**
@@ -39,7 +40,7 @@ public class DataQueryServiceAdapterIntegration {
 
     @Test
     public void queryTradeDataTest() {
-        String tradeData = dataQueryService.queryTradeData(obtainDataRuleList(), obtainEventData());
+        String tradeData = dataQueryService.queryTradeData(obtainDataRuleList(), obtainEventData("paycore"));
         Assert.assertNotNull(tradeData);
     }
 
@@ -57,7 +58,14 @@ public class DataQueryServiceAdapterIntegration {
         return dataRuleList;
     }
 
-    private String obtainEventData() {
-        return "{\"id\":2340516932,\"partnerid\":0,\"business_type\":1,\"split_funds_records\":null,\"merchantno\":\"22010191646623441\",\"mohash\":2526105,\"notifyurl\":\"thrift://com.sankuai.pay.merchantproduct.mwalletcore:3426\",\"name\":\"于娓\",\"acctype\":2,\"money\":456577,\"city\":320300,\"bank\":3,\"branchid\":53748,\"branchname\":\"中国邮政储蓄银行股份有限公司徐州市分行营业部\",\"account\":0,\"fkaccount\":19,\"fixchannel\":0,\"channel\":0,\"comment\":\"\",\"reason\":\"\",\"status\":64,\"notifystatus\":0,\"addtime\":1603095393,\"sendtime\":1603095393,\"recvtime\":1603095395,\"succtime\":1603095396,\"failtime\":0,\"notifytime\":0,\"modtime\":1603095395,\"priority\":1603102593,\"extended_data\":\"{\\\"transIdForCheckingAccount\\\":\\\"\\\",\\\"feeBizCode\\\":\\\"2005\\\",\\\"isTransfer\\\":false,\\\"billMerchantOrderId\\\":\\\"22010191646623441\\\",\\\"batchTransferInfoList\\\":\\\"[]\\\",\\\"withdrawReqType\\\":0,\\\"needNotifyClearingCenter\\\":true,\\\"unwithdrawToPayTransferWay\\\":\\\"actTransfer\\\",\\\"needNotifyTradeDataCenter\\\":true,\\\"billTradeType\\\":\\\"ZJ03\\\",\\\"billTradeId\\\":\\\"2340516932\\\",\\\"noAvailableCountMoney\\\":0,\\\"final_pay_company_code\\\":\\\"QDB\\\",\\\"billTradeChannel\\\":\\\"QD02\\\",\\\"hasSend\\\":\\\"1\\\",\\\"phyChannelCode\\\":\\\"netsunionAcs\\\",\\\"phyChannelName\\\":\\\"网联ACS全渠道\\\",\\\"logicalChannelAccountNo\\\":\\\"991100000111\\\",\\\"logicalChannelId\\\":181,\\\"payCoreMtpp\\\":\\\"mtpp_1095129106_11560614217\\\",\\\"billAccountTime\\\":0}\",\"card_id\":236548639,\"trade_no\":\"1602748863780\",\"iph_pay_merchant_no\":12000102584584,\"queuing_type\":0,\"pay_to_card_biz_req_code\":2005,\"pay_prod_type\":1,\"pay_to_card_account_type\":101,\"from_sub_account_id\":0,\"fee_code\":\"\",\"order_time\":1603095393000,\"fee\":0,\"return_fee\":1,\"start_pay_date\":0,\"error_code\":\"FT2O0000\",\"error_msg\":\"付款成功\"}";
+    private String obtainEventData(String code) {
+        if(code.equals("paycore")) {
+            return PAYCORE_TRADEDATA;
+        }else if(code.equals("transfer")) {
+            return TRANSFER_TRADEDATA;
+        }else if(code.equals("split")){
+            return SPLIT_TRADEDATA;
+        }else
+            return null;
     }
 }

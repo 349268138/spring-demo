9e47657499ad24730b3e2333f045177093e5f78c
jinpingwang <wangjinping@meituan.com>
2020-12-16 14:43:38 +0800

    modify RuleController decisionSearch method

--- pfsServer/src/main/java/com/meituan/pay/finsecurity/controller/RuleController.java
+++ pfsServer/src/main/java/com/meituan/pay/finsecurity/controller/RuleController.java
@@ -1,5 +1,6 @@
 package com.meituan.pay.finsecurity.controller;
 
+import com.github.pagehelper.Page;
 import com.meituan.funds.simple.util.JacksonUtils;
 import com.meituan.funds.simple.util.LoggerUtils;
 import com.meituan.pay.finsecurity.controller.vo.DropListVo;
@@ -25,6 +26,7 @@ import java.util.Map;
  * @date 2020/12/10 4:15 下午
  */
 @RestController
+@RequestMapping(value = "api")
 public class RuleController {
     Logger logger = LoggerFactory.getLogger(RuleController.class);
 
@@ -34,14 +36,14 @@ public class RuleController {
     @Autowired
     private EventRuleRepo eventRuleRepo;
 
-    @RequestMapping(value = "api/decision-search")
+    @RequestMapping(value = "decision-search")
     public String decisionSearch(DecisionRule decisionRule,
                                  @RequestParam(value = "pageNum") int pageNum,
                                  @RequestParam(value = "pageSize") int pageSize){
         Map<String, Object> decisionRuleMap = new HashMap<>();
         try{
-            List<DecisionRule> decisionRuleList = decisionRuleRepo.selectExampleByPage(decisionRule, pageNum, pageSize);
-            decisionRuleMap = obtainSuccessResult(decisionRuleList);
+            Map<String, Object> searchData = obtainSearchData(decisionRule, pageNum, pageSize);
+            decisionRuleMap = obtainSuccessResult(searchData);
             logger.info("decisionRule query succeeded, decisionRule: {}, pageNum: {}, pageSize: {}, decisionRuleMap: {}", decisionRule, pageNum, pageSize, decisionRuleMap);
         } catch (Exception e) {
             decisionRuleMap = obtainErrorResult(e.getMessage());
@@ -51,7 +53,7 @@ public class RuleController {
 
     }
 
-    @RequestMapping(value = "api/decision-add")
+    @RequestMapping(value = "decision-add")
     public String decisionAdd(DecisionRule decisionRule){
         Map<String, Object> decisionRuleMap = new HashMap<>();
         try{
@@ -65,7 +67,7 @@ public class RuleController {
         return JacksonUtils.toJson(decisionRuleMap);
     }
 
-    @RequestMapping(value = "api/decision-update")
+    @RequestMapping(value = "decision-update")
     public String decisionUpdate(DecisionRule decisionRule){
         Map<String, Object> decisionRuleMap = new HashMap<>();
         try{
@@ -79,7 +81,7 @@ public class RuleController {
         return JacksonUtils.toJson(decisionRuleMap);
     }
 
-    @RequestMapping(value = "api/decision-delete")
+    @RequestMapping(value = "decision-delete")
     public String decisionDelete(DecisionRule decisionRule){
         Map<String, Object> decisionRuleMap = new HashMap<>();
         try{
@@ -94,7 +96,7 @@ public class RuleController {
         return JacksonUtils.toJson(decisionRuleMap);
     }
 
-    @RequestMapping(value = "api/event-search-all-code")
+    @RequestMapping(value = "event-search-all-code")
     public String eventSearchAllCode() {
         Map<String, Object> eventRuleMap = new HashMap<>();
         try{
@@ -131,5 +133,13 @@ public class RuleController {
             dropListVos.add(dropListVo);
         }
         return dropListVos;
+    }
+
+    private Map<String, Object> obtainSearchData(DecisionRule decisionRule, int pageNum, int pageSize) {
+        Page page = decisionRuleRepo.selectExampleByPage(decisionRule, pageNum, pageSize);
+        Map<String, Object> searchData = new HashMap<>();
+        searchData.put("total", page.getTotal());
+        searchData.put("list", page);
+        return searchData;
     }
 }
--- pfsServer/src/test/java/com/meituan/pay/finsecurity/controller/RuleControllerTest.java
+++ pfsServer/src/test/java/com/meituan/pay/finsecurity/controller/RuleControllerTest.java
@@ -1,7 +1,25 @@
 package com.meituan.pay.finsecurity.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.github.pagehelper.Page;
+import com.meituan.funds.simple.util.JacksonUtils;
+import com.meituan.pay.finsecurity.dao.repository.DecisionRuleRepo;
+import com.meituan.pay.finsecurity.dao.repository.EventRuleRepo;
+import com.meituan.pay.finsecurity.po.DecisionRule;
+import com.meituan.pay.finsecurity.po.EventRule;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
 
 /**
  * @author hhhb
@@ -9,7 +27,85 @@ import org.slf4j.LoggerFactory;
  */
 public class RuleControllerTest {
 
-    Logger logger = LoggerFactory.getLogger(RuleControllerTest.class);
+    @InjectMocks
+    private RuleController ruleController;
 
+    @Mock
+    private DecisionRuleRepo decisionRuleRepo;
 
+    @Mock
+    private EventRuleRepo eventRuleRepo;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void test001DecisionSearch() {
+        Page page = new Page();
+        page.setTotal(10);
+        when(decisionRuleRepo.selectExampleByPage(any(DecisionRule.class), anyInt(), anyInt())).thenReturn(page);
+        Map<String, Object> result = JacksonUtils.jsonToMap(ruleController.decisionSearch(new DecisionRule(), 1, 10));
+        Assert.assertTrue("0".equals(String.valueOf(result.get("code"))));
+
+        RuntimeException runtimeException = new RuntimeException("selectExampleByPage error");
+        when(decisionRuleRepo.selectExampleByPage(any(DecisionRule.class), anyInt(), anyInt())).thenThrow(runtimeException);
+        result = JacksonUtils.jsonToMap(ruleController.decisionSearch(new DecisionRule(), 1, 10));
+        Assert.assertTrue("1".equals(String.valueOf(result.get("code"))));
+    }
+
+    @Test
+    public void test002DecisionAdd() {
+        when(decisionRuleRepo.insertBySelective(any(DecisionRule.class))).thenReturn(1);
+        Map<String, Object> result = JacksonUtils.jsonToMap(ruleController.decisionAdd(new DecisionRule()));
+        Assert.assertTrue("0".equals(String.valueOf(result.get("code"))));
+
+        RuntimeException runtimeException = new RuntimeException("insertBySelective error");
+        when(decisionRuleRepo.insertBySelective(any(DecisionRule.class))).thenThrow(runtimeException);
+        result = JacksonUtils.jsonToMap(ruleController.decisionAdd(new DecisionRule()));
+        Assert.assertTrue("1".equals(String.valueOf(result.get("code"))));
+    }
+
+    @Test
+    public void test003DecisionUpdate() {
+        when(decisionRuleRepo.updateByIdSelective(any(DecisionRule.class))).thenReturn(1);
+        Map<String, Object> result = JacksonUtils.jsonToMap(ruleController.decisionUpdate(new DecisionRule()));
+        Assert.assertTrue("0".equals(String.valueOf(result.get("code"))));
+
+        RuntimeException runtimeException = new RuntimeException("updateByIdSelective error");
+        when(decisionRuleRepo.updateByIdSelective(any(DecisionRule.class))).thenThrow(runtimeException);
+        result = JacksonUtils.jsonToMap(ruleController.decisionUpdate(new DecisionRule()));
+        Assert.assertTrue("1".equals(String.valueOf(result.get("code"))));
+    }
+
+    @Test
+    public void test004DecisionDelete() {
+        when(decisionRuleRepo.updateByIdSelective(any(DecisionRule.class))).thenReturn(1);
+        Map<String, Object> result = JacksonUtils.jsonToMap(ruleController.decisionDelete(new DecisionRule()));
+        Assert.assertTrue("0".equals(String.valueOf(result.get("code"))));
+
+        RuntimeException runtimeException = new RuntimeException("updateByIdSelective error");
+        when(decisionRuleRepo.updateByIdSelective(any(DecisionRule.class))).thenThrow(runtimeException);
+        result = JacksonUtils.jsonToMap(ruleController.decisionDelete(new DecisionRule()));
+        Assert.assertTrue("1".equals(String.valueOf(result.get("code"))));
+    }
+
+    @Test
+    public void test005EventSearchAllCode() {
+        List<EventRule> eventRuleList = new ArrayList<>();
+        EventRule eventRule = new EventRule();
+        eventRule.setId(1L);
+        eventRule.setCode("1");
+        eventRuleList.add(eventRule);
+        when(eventRuleRepo.selectAll()).thenReturn(eventRuleList);
+
+        Map<String, Object> result = JacksonUtils.jsonToMap(ruleController.eventSearchAllCode());
+        Assert.assertTrue("0".equals(String.valueOf(result.get("code"))));
+
+        RuntimeException runtimeException = new RuntimeException("selectAll error");
+        when(eventRuleRepo.selectAll()).thenThrow(runtimeException);
+        result = JacksonUtils.jsonToMap(ruleController.eventSearchAllCode());
+        Assert.assertTrue("1".equals(String.valueOf(result.get("code"))));
+    }
 }

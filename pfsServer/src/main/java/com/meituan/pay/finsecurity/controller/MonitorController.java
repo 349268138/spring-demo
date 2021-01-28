d9907296d136016e5ae61d11caa74b1a30e7e589
jinpingwang <wangjinping@meituan.com>
2021-01-28 21:12:26 +0800

    update pfs-sdk version to 1.0.3

--- pfsServer/src/main/java/com/meituan/pay/finsecurity/controller/MonitorController.java
+++ pfsServer/src/main/java/com/meituan/pay/finsecurity/controller/MonitorController.java
@@ -1,16 +1,72 @@
 package com.meituan.pay.finsecurity.controller;
 
+import com.dianping.cat.util.MetricHelper;
+import com.meituan.funds.simple.util.JacksonUtils;
+import com.meituan.pay.finsecurity.constant.ScriptConstant;
+import com.meituan.pay.finsecurity.po.ContextData;
+import com.meituan.pay.finsecurity.po.EventRule;
+import com.meituan.pay.finsecurity.po.Vector;
+import com.meituan.pay.finsecurity.script.GroovyScript;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 @RestController
 public class MonitorController {
+    private static final String NAME_ALARM_DECISION = "alarmDecision";
+    private static final String TAG_ALIAS_DECISION = "aliasDecision";
+
     @RequestMapping(value = "monitor/alive")
     @ResponseBody
     public String alive() {
+        EventRule eventRule = new EventRule();
+        List<Vector> vectorList = new ArrayList<>();
+        Vector vector = new Vector();
+        vector.setAlias("partner");
+        vector.setExpr("");
+        vectorList.add(vector);
+
+        Vector vector2 = new Vector();
+        vector2.setAlias("status");
+        vector2.setExpr("");
+        vectorList.add(vector2);
+
+        eventRule.setVectorList(vectorList);
+
+        ContextData contextData = new ContextData();
+        contextData.setEventData("{\"id\":99999999999,\"partnerid\":0");
+        contextData.setTradeData("");
+        String dataJson = JacksonUtils.toJson(contextData);
+
+        MetricHelper pHelper = buildMetricHelper(eventRule, dataJson);
+        if (Objects.isNull(pHelper)) {
+            return "ERROR";
+        }
+
+        pHelper.tag(TAG_ALIAS_DECISION, "alarmMoney");
+        pHelper.name(NAME_ALARM_DECISION).count(1);
+
         return "OK";
     }
+
+    MetricHelper buildMetricHelper(EventRule eventRule, String dataJson) {
+        if (CollectionUtils.isEmpty(eventRule.getVectorList())) {
+            return null;
+        }
+
+        MetricHelper pHelper = MetricHelper.build();
+        for (Vector vector : eventRule.getVectorList()) {
+            String value = String.valueOf(GroovyScript.script(ScriptConstant.CONTEXT_DATA, dataJson, vector.getExpr()));
+            if(ScriptConstant.NULL.equals(value)) {
+                continue;
+            }
+            pHelper.tag(vector.getAlias(), value);
+        }
+        return pHelper;
+    }
 }
-
-

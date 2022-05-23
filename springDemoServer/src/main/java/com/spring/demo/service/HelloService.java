4088570337fabe6db219c67d2b2d3ab7ceecdc85
jinpingwang <wangjinping@meituan.com>
2022-05-23 17:44:26 +0800

    config db.properties

--- springDemoServer/src/main/java/com/spring/demo/service/HelloService.java
+++ springDemoServer/src/main/java/com/spring/demo/service/HelloService.java
@@ -6,21 +6,11 @@ package com.spring.demo.service;
  * @CreateDateon 2021/12/12.
  */
 public class HelloService {
-    public String getEnvName() {
-        return envName;
-    }
-
-    public void setEnvName(String envName) {
-        this.envName = envName;
-    }
-
-    private String envName;
-
     public HelloService() {
         System.out.println("HelloService create");
     }
 
     public String welcome(String name) {
-        return name + ", 你好啊, " + envName;
+        return name + ", 你好啊";
     }
 }
